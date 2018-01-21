0d23ea16b170684263c1bf55bfb4fff646837e84
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-01-21 18:53:55 -0500

    basic previous button functionality added

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -11,7 +11,17 @@
     const nextSongIndex = currentSongIndex + 1;
     const nextSong = album.songs[nextSongIndex];
     if(nextSongIndex >= album.songs.length) { return; }
-    
+
     player.playPause(nextSong);
-  });
+    });
+
+    $('button#previous').on('click', function () {
+      if (player.playState !== 'playing') { return; }
+
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      const previousSong = album.songs[previousSongIndex];
+
+      player.playPause(previousSong);
+    });
 }
